b16ccb3989fa596c09a457f1b65c6bb98630c8c6
santoshkakkar <santosh.kakkar@hva.nl>
2023-09-27 22:14:03 +0200

    werkend

--- frontend/auc-vue/src/main.js
+++ frontend/auc-vue/src/main.js
@@ -2,11 +2,11 @@ import { createApp } from 'vue'
 import App from './AppComponent31.vue'
 import './assets/global.css'
 import router from "@/router";
-import {createRouter, createWebHistory} from "vue-router";
-const router = createRouter({
-    history: createWebHistory(),
-    routes:[{
-
-    }]
-})
+// import {createRouter, createWebHistory} from "vue-router";
+// const router = createRouter({
+//     history: createWebHistory(),
+//     routes:[{
+//
+//     }]
+// })
 createApp(App).use(router).mount('#app');
--- frontend/auc-vue/src/router/index.js
+++ frontend/auc-vue/src/router/index.js
@@ -1,23 +1,23 @@
-
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+//
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
 
 // Importing the provided .vue components
-import OffersDetail32 from '@/components/offers/OffersDetail32';
-import OffersOverview31 from '@/components/offers/OffersOverview31';
-import OffersOverview32 from '@/components/offers/OffersOverview32';
+// import OffersDetail32 from '@/components/offers/OffersDetail32';
+// import OffersOverview31 from '@/components/offers/OffersOverview31';
+// import OffersOverview32 from '@/components/offers/OffersOverview32';
 
-Vue.use(VueRouter);
+// Vue.use(VueRouter);
 
-const routes = [
-    { path: '/offers-detail', component: OffersDetail32 },
-    { path: '/offers-overview-31', component: OffersOverview31 },
-    { path: '/offers-overview-32', component: OffersOverview32 }
-];
+// const routes = [
+//     { path: '/offers-detail', component: OffersDetail32 },
+//     { path: '/offers-overview-31', component: OffersOverview31 },
+//     { path: '/offers-overview-32', component: OffersOverview32 }
+// ];
 
-const router = new VueRouter({
-    mode: 'hash',
-    routes
-});
+// const router = new VueRouter({
+//     mode: 'hash',
+//     routes
+// });
 
-export default router;
+// export default router;
